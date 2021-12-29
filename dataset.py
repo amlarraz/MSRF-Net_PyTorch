Fix somo dataset bug and add the accumulative gradient option

dataset.py
@@ -53,7 +53,7 @@ class DataSet(Dataset):
             self.augs = albu.OneOf([albu.ElasticTransform(p=0.5, alpha=120, sigma=280 * 0.05, alpha_affine=120 * 0.03),
                                     albu.GridDistortion(p=0.5, border_mode=cv2.BORDER_CONSTANT, distort_limit=0.2),
                                     albu.Rotate(p=0.5, limit=(-5, 5), interpolation=1, border_mode=cv2.BORDER_CONSTANT),
-                                    ])
+                                    ],)
     def __len__(self):
         return len(self.img_names)
 
@@ -77,13 +77,14 @@ class DataSet(Dataset):
             img = cv2.resize(img, self.resize, interpolation=cv2.INTER_CUBIC)
             msk = cv2.resize(msk, self.resize, interpolation=cv2.INTER_NEAREST)
 
-        canny = cv2.Canny(img, 10, 100)
-        canny = np.asarray(canny, np.float32)
-        canny /= 255.0
         if self.augmentation:
             augmented = self.augs(image=img, mask=msk)
             img = augmented['image']
             msk = augmented['mask']
+
+        canny = cv2.Canny(img, 10, 100)
+        canny = np.asarray(canny, np.float32)
+        canny /= 255.0
 
         img = normalize(img)
 

metrics.py
@@ -7,11 +7,11 @@ from losses import one_hot
 def calculate_dice(pred, msk, eps=1e-6):
 
     # compute softmax over the classes axis
-    input_soft = F.softmax(pred, dim=1)
+    input_soft = F.softmax(pred, dim=1)[:, 1:]
 
     # create the labels one hot tensor
     target_one_hot = one_hot(msk, num_classes=pred.shape[1],
-                             device=pred.device, dtype=pred.dtype)  # [:, 1:]
+                             device=pred.device, dtype=pred.dtype)[:, 1:]
 
     # compute the actual dice score
     dims = (1, 2, 3)

train.py
@@ -18,7 +18,8 @@ resize = (256, 256)  # None or 2-tuple
 
 n_epochs = 100
 batch_size = 3
-lr = 2e-4
+lr = 1e-4
+accumulation_steps = 6
 weight_decay = 0.01
 device = torch.device('cuda:0')
 
@@ -53,12 +54,17 @@ for epoch in range(1, n_epochs+1):
 
     for i, (img, canny, msk, canny_label) in enumerate(dataloader_train):
         img, canny, msk, canny_label = img.to(device), canny.to(device), msk.to(device), canny_label.to(device)
-        optimizer.zero_grad()
+
         pred_3, pred_canny, pred_1, pred_2 = model(img, canny)
         # Forward + Backward + Optimize
         loss = criterion(pred_3, pred_canny, pred_1, pred_2, msk, canny_label)
+        loss = loss/accumulation_steps
         loss.backward()
-        optimizer.step()
+        # accumulative gradient
+        if (i + 1) % accumulation_steps == 0:  # Wait for several backward steps
+            optimizer.step()  # Now we can do an optimizer step
+            model.zero_grad()  # Reset gradients tensors
+
         metrics['train_loss'].append(loss.item())
         dice = calculate_dice(pred_3, msk)
         metrics['train_dice'].append(dice.item())
@@ -84,7 +90,7 @@ for epoch in range(1, n_epochs+1):
             tq.update(batch_size)
 
             if k < n_img_to_tb:
-                imgs2tb(img, msk, pred_3, writer, k, epoch+1)
+                imgs2tb(img, msk, pred_3, canny, pred_canny, writer, k, epoch+1)
                 k += 1
 
     print('Epoch {}: val loss: {}, val dice: {}'.format(epoch, np.mean(metrics['val_loss']), np.mean(metrics['val_dice'])))

utils.py
@@ -23,7 +23,7 @@ def prepare_writer(dataset_name):
     return writer, os.path.join('./logs', log_name)
 
 
-def imgs2tb(img, msk, pred, writer, n_img, epoch):
+def imgs2tb(img, msk, pred, canny, pred_canny, writer, n_img, epoch):
     n_classes = pred.shape[1]
     # It shows only the 1st img in the batch
     # Prepare data
@@ -33,7 +33,10 @@ def imgs2tb(img, msk, pred, writer, n_img, epoch):
     msk = msk[0].detach().cpu().numpy()*(255//n_classes)          # to show in bright colors
     pred = torch.argmax(F.softmax(pred[0], dim=0), dim=0).detach().cpu().numpy()*(255//n_classes)  # No threshold->argmax
 
-    final_img = np.concatenate([img, msk, pred], axis=1)
+    canny = canny[0, 0].detach().cpu().numpy()*50
+    pred_canny = pred_canny[0, 0].detach().cpu().numpy()*50
+
+    final_img = np.concatenate([img, msk, pred, canny, pred_canny], axis=1)
     writer.add_image('Image {}'.format(n_img), final_img, epoch, dataformats='HW')
 
     return None